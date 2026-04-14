ad03dd4c4dd9373e0354d604ec9be9149d9679cc
srihith kancharla <srihithkancharla3@gmail.com>
2026-04-14 19:20:41 +0530

    q5.s done

--- q5/q5.s
+++ q5/q5.s
@@ -4,7 +4,8 @@ file_name:
 output_yes:
         .string "Yes\n"
 output_no:
-         .string "No\n"
+        .string "No\n"
+
 .section .text
 .globl main
 
@@ -19,44 +20,44 @@ main:
         addi x17,x0,56
         ecall
 
-        add x18,x10,x0
+        addi x18,x10,0
 
-        add x10,x18,x0
-        add x11,x0,x0
+        addi x10,x18,0
+        addi x11,x0,0
         addi x12,x0,2
         addi x17,x0,62
         ecall
 
-        add x19,x0,x0    #left pointer
+        addi x19,x0,0    #left
 
-        add x20,x10,x0
-        addi x20,x20,-1  #right pointer
+        addi x20,x10,-2  #right
 
-        add x24,x0,x0   #counter
+        addi x24,x0,0    #counter
+
 loop:
 
-        ble x20,x19,check
+        bge x19,x20,check
 
-        add x10,x18,x0
+        addi x10,x18,0
         addi x11,x19,0
         addi x12,x0,0
         addi x17,x0,62
         ecall
 
-        add x10,x18,x0
+        addi x10,x18,0
         addi x11,sp,0
         addi x12,x0,1
         addi x17,x0,63
         ecall
         lb x22,0(sp)
 
-        add x10,x18,x0
+        addi x10,x18,0
         addi x11,x20,0
         addi x12,x0,0
         addi x17,x0,62
         ecall
 
-        add x10,x18,x0
+        addi x10,x18,0
         addi x11,sp,0
         addi x12,x0,1
         addi x17,x0,63
@@ -64,6 +65,7 @@ loop:
         lb x23,0(sp)
 
         bne x22,x23,inc_cnt
+        j continue
 
 inc_cnt:
         addi x24,x24,1
@@ -77,7 +79,7 @@ continue:
 check:
 
         beq x24,x0,YES
-        la x10,output_yes
+        la x10,output_no
         call printf
         j closing
 
